b21da5378dec07f3aadd37e6c80ac86e0ed73240
DESKTOP-F9CUFM9\user <freshkimdh@gmail.com>
2020-11-07 11:59:29 +0900

    배포 1.0

--- src/main/java/com/wmk/ex/controller/HomeController.java
+++ src/main/java/com/wmk/ex/controller/HomeController.java
@@ -1,11 +1,17 @@
 package com.wmk.ex.controller;
 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.wmk.ex.service.UserService;
 
@@ -19,6 +25,20 @@ public class HomeController {
 	@Autowired
 	private UserService userService;
 	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		log.info("Welcome home! The client locale is {}.");
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
 	@GetMapping(value="index")
 	public String index(Model model) {
 		log.info("index...");
@@ -26,8 +46,7 @@ public class HomeController {
 		return "/wmk_home/index";
 	}
 	
-	
-	
+
 	@GetMapping("/characterMaking")
 	public String character(Model model) {
 		log.info("character...");
